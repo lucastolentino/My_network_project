8276aa7a3ccdcf5b11d945e3a0d0c23bcf142ae6
lucastolentino <lucasalvestolentino@gmail.com>
2022-05-10 21:55:46 -0300

    Criando nosso arquivo padrao sasse e começando a estilizar o header

--- cache/header.6b2b9eed27617396b15614b446e03c68.rtpl.php
+++ cache/header.6b2b9eed27617396b15614b446e03c68.rtpl.php
@@ -1,24 +1,28 @@
 <?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE HTML>
-<html lang=“pt-br”>
+<html lang="pt-br">
 <head>
-<meta charset=“utf-8”/>
-<meta content=“width=device-width, initial-scale=1, maximum-scale=1” name=“viewport”>
+<meta charset="utf-8"/>
+<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
 <title><?php echo htmlspecialchars( $title_pagina, ENT_COMPAT, 'UTF-8', FALSE ); ?></title>
-<link href=“css/seu-stylesheet.css” rel=“stylesheet”/>
-<script src=“scripts/seu-script.js”></script>
+<link href="resources/css/css.css" rel="stylesheet"/>
+<script src="resources/js/js.min.js"></script>
 </head>
 <body>
 <header>
-	<div class="logo">
-		<img src="resources/imagens/logo_principal.png">
-	</div>
-	<div class="login">
-		<form action="login" method="POST">
-			<input type="email" name="email">
-			<input type="password" name="senha">
-			<input type="submit" name="btn" value="Entrar"> 
+	<div class="content">
 			
-		</form>
+	  <div class="logo">
+		   <img src="resources/imagens/logo_principal.png">
+	  
+	  </div>
+	        <div class="login">
+		        <form action="login" method="POST">
+
+			        <input type="email" name="email">
+			        <input type="password" name="senha">
+			        <input type="submit" name="btn" value="Entrar"> 
+		        </form>
 		
+	        </div>
 	</div>
 </header>
--- system/views/header.php
+++ system/views/header.php
@@ -1,24 +1,28 @@
 <!DOCTYPE HTML>
-<html lang=“pt-br”>
+<html lang="pt-br">
 <head>
-<meta charset=“utf-8”/>
-<meta content=“width=device-width, initial-scale=1, maximum-scale=1” name=“viewport”>
+<meta charset="utf-8"/>
+<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
 <title>{$title_pagina}</title>
-<link href=“css/seu-stylesheet.css” rel=“stylesheet”/>
-<script src=“scripts/seu-script.js”></script>
+<link href="resources/css/css.css" rel="stylesheet"/>
+<script src="resources/js/js.min.js"></script>
 </head>
 <body>
 <header>
-	<div class="logo">
-		<img src="resources/imagens/logo_principal.png">
-	</div>
-	<div class="login">
-		<form action="login" method="POST">
-			<input type="email" name="email">
-			<input type="password" name="senha">
-			<input type="submit" name="btn" value="Entrar"> 
+	<div class="content">
 			
-		</form>
+	  <div class="logo">
+		   <img src="resources/imagens/logo_principal.png">
+	  
+	  </div>
+	        <div class="login">
+		        <form action="login" method="POST">
+
+			        <input type="email" name="email">
+			        <input type="password" name="senha">
+			        <input type="submit" name="btn" value="Entrar"> 
+		        </form>
 		
+	        </div>
 	</div>
 </header>
