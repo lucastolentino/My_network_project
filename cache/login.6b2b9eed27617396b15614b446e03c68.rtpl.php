d71bcbf97c61e0bd8ec3fdef81d8c6605001893d
lucastolentino <lucasalvestolentino@gmail.com>
2022-05-25 14:01:31 -0300

    Criando a estruturua HTML da pagina de login

--- cache/login.6b2b9eed27617396b15614b446e03c68.rtpl.php
+++ cache/login.6b2b9eed27617396b15614b446e03c68.rtpl.php
@@ -1,5 +1,15 @@
 <?php if(!class_exists('Rain\Tpl')){exit;}?> <section>
- 	 Minha página de login
+ 	<form action="/" method="POST">
+        <legend> Cadastrar</legend>
+        <input type="text" name="nome" placeholder="Nome" >
+        <input type="mail" name="email" placeholder="E-mail">
+        <input type="text" name="telefone" placeholder="Telefone">
+        <input type="password" name="senha" placeholder="Senha">
+        <p>Ao se cadastrar você concorda com os <a href="#">termos e política</a></p>
+        <input type="submit" name="btn" value="Cadastrar">
+
+
+    </form>
  </section>	
 
 
